a50ecc4f570fa529429281d7975805798ea7f98e
alex olivares <alexander.b.olivares@gmail.com>
2021-11-21 18:50:53 -0600

    db connection & get nfl endpoint

---
+++ db.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const Pool = require("pg").Pool;
+
+const devConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
+
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const PORT = process.env.PORT || 5000;
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
+
+router.get("/nfl-week/:week", async (req, res) => {
+  const { week } = req.params;
+  try {
+    let getPredictions = await pool.query(`SELECT * FROM nfl_week_${week}`);
+    res.json(getPredictions.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.send(`No predictionsn for NFL week "${week}"`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
