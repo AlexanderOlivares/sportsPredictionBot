5a508aef3236b034c6e2a0947f110ebd9016f271
alex olivares <alexander.b.olivares@gmail.com>
2022-01-23 13:04:52 -0600

    nfl season active weeks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const pool = require("./db");
 const PORT = process.env.PORT || 5000;
