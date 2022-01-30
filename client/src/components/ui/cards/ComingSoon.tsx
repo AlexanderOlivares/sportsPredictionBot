7d9b24d52191eb1c4f28e9daf2dac1e47a989693
alex olivares <alexander.b.olivares@gmail.com>
2022-01-30 12:36:03 -0600

    faviconadded

--- client/src/components/ui/cards/ComingSoon.tsx
+++ client/src/components/ui/cards/ComingSoon.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import HandymanOutlinedIcon from "@mui/icons-material/HandymanOutlined";
 import useMediaQuery from "../../helpers/UseMediaQuery";
+import Grid from "@mui/material/Grid";
 
 interface ComingSoonProps {
   message: string;
@@ -14,19 +14,21 @@ const ComingSoonCard: React.FC<ComingSoonProps> = ({ message }) => {
   const isMobile = useMediaQuery("(max-width:600px)");
   return (
     <>
-      <Box m={2}>
-        <Card variant="outlined" sx={{ m: 1, width: isMobile ? 345 : 550 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Coming Soon!
-            </Typography>
-            <HandymanOutlinedIcon sx={{ fontSize: 150 }} />
-            <Typography variant="body2" color="text.secondary">
-              {message}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
+      <Grid container columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center">
+        <Grid item lg={2}>
+          <Card variant="outlined" sx={{ m: 1, width: isMobile ? 345 : 550 }}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Coming Soon!
+              </Typography>
+              <HandymanOutlinedIcon sx={{ fontSize: 150 }} />
+              <Typography variant="body2" color="text.secondary">
+                {message}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 };
